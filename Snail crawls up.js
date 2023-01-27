81c6c67240334f1064313276d449b1eb0feef25f
Thomas Ableson <80835744+spiritumbr@users.noreply.github.com>
2023-01-27 08:44:30 -0500

    reuploaded

--- Snail crawls up.js
+++ Snail crawls up.js
@@ -1,3 +1,12 @@
 function snail(column, day, night) {
-  // your code here
+    let days = 0;
+    let currentHeight = 0;
+    while(currentHeight !== column){
+        currentHeight += day;
+        days++;
+        if(currentHeight >= column){
+            return days;
+        }
+        currentHeight -= night;
+    }
 }
