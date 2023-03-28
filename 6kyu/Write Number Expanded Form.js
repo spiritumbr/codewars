6fea99c1ea70bb1cc719c7a38564e763f4f16181
Thomas Ableson <80835744+spiritumbr@users.noreply.github.com>
2023-03-28 12:19:46 -0400

    Completed 'Write Number Expanded Form'

---
+++ 6kyu/Write Number Expanded Form.js
@@ -0,0 +1,11 @@
+function expandedForm(num) {
+  let numStr = num.toString();
+  let result = [];
+  for (let i = 0; i < numStr.length; i++) {
+    if (numStr[i] !== '0') {
+      let zeros = '0'.repeat(numStr.length - i - 1);
+      result.push(numStr[i] + zeros);
+    }
+  }
+  return result.join(' + ');
+}
